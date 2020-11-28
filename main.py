829982dfb119ba80f52ce629644934669dac12ed
itsmeeugene <evgenij_Fragolit_pavlov@mail.ru>
2020-11-28 19:13:58 +0300

    -

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Object:
         self.vx = vx
         self.vy = vy
 
-    def recalculate(self, a_x, a_y):
+    def recalculate(self, a_x=0.0, a_y=0.0):
         self.x += self.vx * step + a_x * step ** 2 / 2
         self.y += self.vy * step + a_y * step ** 2 / 2
 
@@ -35,21 +35,27 @@ def dist(p1, p2):
 
 
 G = 6.67 * 10 ** -11
-m1, m2, vx1, vy1, vx2, vy2, x1, y1, x2, y2 = 100, 5.9722 * 10 ** 24, 0, 7844, 0, 0, -6371000, 0, 0, 0
+m1, m2 = 100, 5.9722 * 10 ** 24
+vx1, vy1 = 0, 12844
+vx2, vy2 = 0, 5000
+x1, y1 = -6371000, 0
+x2, y2 = 0, 0
+
 planet1 = Object(m1, x1, y1, vx1, vy1)
 planet2 = Object(m2, x2, y2, vx2, vy2)
 coords = []  # сохраняем траекторию
 
 t = 0
-step = 40
-n = 100
+step = 50
+n = 150
 name = "photos_" + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
 
 r = dist(planet1, planet2)
 F = G * planet1.m * planet2.m / r ** 2
-e = planet1.m * (planet1.vx ** 2 + planet1.vy ** 2) / 2 - F * r
+
 
 for i in range(n):
+    e = planet1.m * (planet1.vx ** 2 + planet1.vy ** 2) / 2 - F * r
     r = dist(planet1, planet2)
     F = G * planet1.m * planet2.m / r ** 2
     v = sqrt(2 * e / planet1.m + 2 * G * planet2.m / r)
@@ -81,14 +87,29 @@ for i in range(n):
         else:
             planet1.vx = sqrt(v * v - planet1.vy ** 2)
 
-    print("v = ", v, "vx = ", planet1.vx, "vy = ", planet1.vy, "ax = ", ax, "ay = ", ay, "alpha = ", alpha, "frame = ", i)
+    planet2.recalculate()
+
+    print("v = ", v, "vx = ", planet1.vx, "vy = ", planet1.vy, "ax = ", ax, "ay = ", ay, "alpha = ", alpha, "e = ", e)
 
     coords.append((planet1.x, planet1.y))
 
     current = os.getcwd()
     if not os.path.exists("{}/{}".format(current, name)):
         os.makedirs("{}/{}".format(current, name))
-    plt.axis([-10000000, 10000000, -10000000, 10000000])
+    # plt.axis([-10000000, 10000000, -3000000, 50000000])
+
+    left, width = .25, .5
+    bottom, height = .25, .5
+    right = left + width
+    top = bottom + height
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(2)
+    ax.text(right, top, '               e = {}'.format(round(e)),
+            horizontalalignment='left',
+            verticalalignment='top',
+            transform=ax.transAxes)
     plt.scatter(planet1.x, planet1.y, 100)
     plt.scatter(planet2.x, planet2.y, 2000)
     for coord in coords:
@@ -103,3 +124,4 @@ with imageio.get_writer('{}/{}/satellite.gif'.format(current, name), mode='I') a
         filename = str(i) + ".png"
         image = imageio.imread('{}/{}/{}'.format(current, name, filename))
         writer.append_data(image)
+
